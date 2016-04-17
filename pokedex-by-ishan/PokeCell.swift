8f335fd8f1cc73a7c74c5e72df53141a59cc5a48
Ishan Shams <ishanshams@Ishans-MBP.home>
2016-04-16 23:15:58 -0400

    Added Collection View

---
+++ pokedex-by-ishan/PokeCell.swift
@@ -0,0 +1,29 @@
+//
+//  PokeCell.swift
+//  pokedex-by-ishan
+//
+//  Created by Ishan Shams on 4/16/16.
+//  Copyright © 2016 Ishan Shams. All rights reserved.
+//
+
+import UIKit
+
+class PokeCell: UICollectionViewCell {
+    @IBOutlet weak var thumbImg: UIImageView!
+    @IBOutlet weak var nameLbl: UILabel!
+    
+    var pokemon: Pokemon!
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        self.layer.cornerRadius = 5.0
+    }
+    
+    func configureCell(pokemon: Pokemon) {
+        self.pokemon = pokemon
+        
+        nameLbl.text = self.pokemon.name.capitalizedString
+        thumbImg.image = UIImage(named: "\(self.pokemon.pokedexId)")
+        
+    }
+}
